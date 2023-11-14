a68d6d00ac76cbbba9becb0f4dfb8e9bb583bac9
ethancampana <ethancampana@gmail.com>
2023-11-14 17:37:10 -0500

    Basic Game Camera
    game camera scene

---
+++ Scripts/GameCamera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+class_name GameCamera
+# Target node that the camera will follow
+@export var target_node : Node2D = null
+
+# Camera follow speed (adjust as needed)
+var follow_speed : float = 500.0
+
+func _process(delta):
+    if target_node:
+        # Calculate the new camera position based on the target node's position
+        var target_position = target_node.global_position
+        var new_position = position.move_toward(target_position, follow_speed * delta)
+        
+        # Set the camera position to the new calculated position
+        position = new_position
