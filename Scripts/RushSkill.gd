869490ddad92b42299d85723d99a58723de5d816
ethancampana <ethancampana@gmail.com>
2023-11-15 20:19:18 -0500

    cleanup

--- Scripts/RushSkill.gd
+++ Scripts/RushSkill.gd
@@ -3,21 +3,14 @@ class_name RushSkill
 
 
 var cell: Vector2i
-
-
-func _process(_delta):
-    if toggled:
-        tile_selection()
-        if Input.is_action_just_pressed("leftClick"):
-            select_tile(map.local_to_map(map.get_global_mouse_position()))
-        if Input.is_action_just_pressed("rightClick"):
-            toggled = false
+var usedSkill : bool = false
 
 func select_tile(pos):
     if pos != cell:
         return
     apply(champ) 
     toggled = false
+    coolDownActive = true
     GameController.emit_signal("skill_toggled", toggled)
     return
 
@@ -26,5 +19,18 @@ func apply(target):
     target.movement_speed *=2
 
 func tile_selection():
-    cell = map.local_to_map(champ.position)
-    map.set_cell(9,cell,map.get_cell_source_id(1,cell),map.get_cell_atlas_coords(1,cell),0)
+    cell = champ.tile_position
+    map.set_cell(8,cell,0,Vector2i(0,1),0)
+
+
+func _process(_delta):
+    if toggled:
+        if coolDownActive:
+            toggled = false
+            GameController.emit_signal("skill_toggled", toggled)
+        tile_selection()
+        if Input.is_action_just_pressed("leftClick"):
+            select_tile(map.local_to_map(map.get_global_mouse_position()))
+        if Input.is_action_just_pressed("ui_accept") and not usedSkill:
+            toggled = false
+            GameController.emit_signal("skill_toggled", toggled)
