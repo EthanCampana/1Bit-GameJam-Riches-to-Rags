be8ce692dc481bd9df12ea3048113d418d1a856a
Retro5050 <Ethan_Robinson2@baylor.edu>
2023-11-16 22:31:40 +0900

    n/a

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
+@onready var animation_player = $NewGameCollision/AnimationPlayer
+
 var mouseOver = false
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if mouseOver == true:
 			# CHANGE THIS SCENE TO THE CHARACTER SELECT SCENE
-			anim.play("fadeIn")
-			get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+			animation_player.play("fadeIn")
 
 # Mouse is hovering over the element
 func _on_mouse_entered():
@@ -16,3 +17,8 @@ func _on_mouse_entered():
 # Mouse stopped hovering over the element
 func _on_mouse_exited():
 	mouseOver = false
+
+
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
